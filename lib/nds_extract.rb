f38c6880880a1475064bd172008b5467c9b8ff3b
Christine Bumpous <bumpouce@gmail.com>
2020-01-22 22:46:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -44,7 +44,7 @@ def total_gross(source)
   directors = list_of_directors(source)  #names only
   
   while directors_count < directors.length do
-    collection_of_director_grosses[directors[directors_count]] = gross_for_director()
+    collection_of_director_grosses[directors[directors_count]] = gross_for_director(source[directors[directors_count]])
     directors_count += 1
   end 
 end
